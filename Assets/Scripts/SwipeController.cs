8d1c40383b502d3fe1972438feb4d59e34fdef8e
Facundo Lastra <flastra@carnival.com>
2020-02-01 17:10:54 -0300

    player movement basic

---
+++ Assets/Scripts/SwipeController.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeController : MonoBehaviour
+{
+    public SwipeDetection swipeControls;
+    private CharacterController controller;
+    private const float LANE_DISTANCE = 3.0f;
+
+    public double topLeft = -4.5;
+    public double topRight = 4.5;
+
+    public float position = 0f;
+    public float speed = 7f;
+    public float gravity = 12.0f;
+    public float verticalVelocity;
+
+    private int desiredLane = 1; // 0 left , 1 center, 2 right
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (swipeControls.SwipeLeft)
+        {
+            MoveLane(true);
+
+
+        }
+        else if (swipeControls.SwipeRight)
+        {
+            
+            MoveLane(false);
+        }
+        else if (swipeControls.SwipeUp)
+        {
+            
+        }
+
+    
+        // calculate where we should be in the future
+
+        Vector3 targetPosition = transform.position.z * Vector3.forward;
+
+        if( desiredLane == 0)
+        {
+            targetPosition += Vector3.left * LANE_DISTANCE;
+        } else if (desiredLane == 2)
+        {
+            targetPosition += Vector3.right * LANE_DISTANCE;
+        }
+
+
+        // calculate our move delta
+
+        Vector3 moveVector = Vector3.zero;
+        moveVector.x = (targetPosition - transform.position).normalized.x * speed;
+        moveVector.y = -0.1f;
+        moveVector.z = -1 * speed;
+
+        //move character
+        controller.Move(moveVector * Time.deltaTime);
+         
+    }
+
+    private void MoveLane(bool goingRight)
+    {
+        desiredLane += (goingRight) ? 1 : -1;
+        desiredLane = Mathf.Clamp(desiredLane, 0, 2);
+    }
+}
---
+++ Assets/Scripts/SwipeDetection.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwipeDetection : MonoBehaviour
+{
+    private bool tap, swipeLeft, swipeRight, swipeUp;
+    private Vector2 startTouch, swipeDelta;
+
+    private bool isDragin = false;
+
+    public Vector2 SwipeDelta { get { return swipeDelta; }}
+    public bool SwipeLeft {  get { return swipeLeft; } }
+    public bool SwipeRight {  get { return swipeRight; } }
+    public bool SwipeUp { get { return swipeUp; } }
+    public bool Tap { get { return tap; } }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tap = swipeLeft = swipeRight = swipeUp = false;
+
+        // input detection
+        #region Standalone Inputs
+        if(Input.GetMouseButtonDown(0))
+        {
+            tap = true;
+            isDragin = true;
+            startTouch = Input.mousePosition;
+
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDragin = false;
+            Reset();
+        }
+        #endregion
+        #region Standalone Inputs touch
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                isDragin = true;
+                tap = true;
+                startTouch = Input.touches[0].position;
+            }else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDragin = false;
+                Reset();
+            }
+        }
+        #endregion
+
+        // Calculate the distances
+
+        swipeDelta = Vector2.zero;
+        if(isDragin)
+        {
+            if(Input.touches.Length > 0)
+            {
+                swipeDelta = Input.touches[0].position - startTouch;
+            }
+            else if ( Input.GetMouseButton(0))
+            {
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+            }               
+        }
+
+        // Did we cross the deadZone?
+
+        if (swipeDelta.magnitude > 125)
+        {
+            // which direction?
+            float x = swipeDelta.x;
+            float y = swipeDelta.y;
+
+            if(Mathf.Abs(x) > Mathf.Abs(y))
+            {
+
+                //left o right
+                if(x < 0)
+                {
+                    swipeLeft = true;
+                }
+                else
+                {
+                    swipeRight = true;
+                }
+            }
+            else
+            {
+                // Up or down
+                if ( y > 0)
+                {
+                    swipeUp = true;
+                }
+            }
+            Reset();
+        }
+    }
+
+   
+
+
+    private void Reset()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDragin = false;
+    }
+}
