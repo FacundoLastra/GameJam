2c62ac273b7527d29f67cec82315b8aff3e6d5dd
lucas.najle <lucas.najle@globant.com>
2020-02-01 19:55:34 -0300

    Health Controller

---
+++ Assets/OrbCollider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbCollider : MonoBehaviour
+{
+		public GameObject player;
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+
+		void OnTriggerEnter()
+		{
+			player = GameObject.Find("Game");
+			Debug.Log("TRIGGER");
+			HealthController.getInstance();
+			HealthController.getInstance().addHealth(20f);
+			//GetComponent("Health Controller").addHealth(20f);
+		}
+}
---
+++ Assets/Scripts/HealthController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthController : MonoBehaviour
+{
+		public float currentHealth = 100f;
+		public float maxHealth = 200f;
+		public float staticDamage = 1f;
+		public GameObject HealthUI;
+		private static HealthController _instance;
+
+		// Start is called before the first frame update
+		void Start()
+		{
+			if (_instance == null) {
+				_instance = this;
+			} else {
+				Destroy(gameObject);
+			}
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			if (currentHealth < maxHealth) {
+				currentHealth -= staticDamage * Time.deltaTime;
+			};
+			UpdateUI();
+		}
+
+		void UpdateUI() {
+			HealthUI.GetComponent<UnityEngine.UI.Text>().text = currentHealth.ToString();
+		}
+
+		public static HealthController getInstance() {
+			return _instance;
+		}
+
+		public void addHealth(float KeyValuePair) {
+			currentHealth += KeyValuePair;
+		}
+}
