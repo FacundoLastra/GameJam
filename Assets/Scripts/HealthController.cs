f56206f2d4923b716ee89b1f50bd821bff0e08f1
Facundo Lastra <flastra@carnival.com>
2020-02-02 13:20:44 -0300

    mask mecanic implemented

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -9,13 +9,21 @@ public class HealthController : MonoBehaviour
 		public float maxHealth = 200f;
 		public float staticDamage = 1f;
 		public Slider HealthUI;
+		public RawImage MaskImage;
 		private static HealthController _instance;
 	    public Animator deathMenuAnim;
 	    private bool isDead = false;
 	    public SwipeController playerController;
 
-	    // Start is called before the first frame update
-	    void Start()
+	// Mask Textures
+	public Texture maskTexture0;
+	public Texture maskTexture1;
+	public Texture maskTexture2;
+	public Texture maskTexture3;
+	public Texture maskTexture4;
+
+	// Start is called before the first frame update
+	void Start()
 		{
 			if (_instance == null) {
 				_instance = this;
@@ -39,8 +47,20 @@ public class HealthController : MonoBehaviour
 		}
 
 		void UpdateUI() {
-			HealthUI.GetComponent<UnityEngine.UI.Slider>().value = currentHealth;
-		}
+			HealthUI.GetComponent<UnityEngine.UI.Slider>().value = currentHealth;		
+
+		if( currentHealth < 20)
+			MaskImage.GetComponent<RawImage>().texture = maskTexture0;
+		else if (currentHealth < 40)
+			MaskImage.GetComponent<RawImage>().texture = maskTexture1;
+		else if (currentHealth < 60)
+			MaskImage.GetComponent<RawImage>().texture = maskTexture2;
+		else if (currentHealth < 80)
+			MaskImage.GetComponent<RawImage>().texture = maskTexture3;
+		else
+			MaskImage.GetComponent<RawImage>().texture = maskTexture4;
+
+	}
 
 		public static HealthController getInstance() {
 			return _instance;
